bf05ebe06ed5039a44131e27375151b475e3c363
bcuser <jacobsolvang@gmail.com>
2023-10-30 15:08:54 -0700

    Updated navbar to include all pages - Only navigates if signed in.

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserApiService } from '../user-api.service';
 import { MatDialog } from '@angular/material/dialog';
 import { LogoutDialogComponent } from '../logout-dialog/logout-dialog.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,11 +10,32 @@ import { LogoutDialogComponent } from '../logout-dialog/logout-dialog.component'
   styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent implements OnInit {
-  constructor(private dialog: MatDialog) {}
+  constructor(private dialog: MatDialog, private router: Router) {}
 
   ngOnInit(): void {}
 
   Logout() {
     this.dialog.open(LogoutDialogComponent);
+  }
+
+  GoHome() {
+    if (localStorage.getItem('userId') != null) {
+      this.router.navigate(['/choose-view']);
+    }
+    else {
+      this.router.navigate(['/']);
+    }
+  }
+
+  GoList() {
+    if (localStorage.getItem('userId') != null) {
+      this.router.navigate(['/chars']);
+    }
+  }
+
+  GoCreate() {
+    if (localStorage.getItem('userId') != null) {
+      this.router.navigate(['/create-edit']);
+    }
   }
 }
