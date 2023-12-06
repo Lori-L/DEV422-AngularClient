2770103ae8a3a2724bbd7d4bcfa9442efe9249d4
Lori-L <tokyface@gmail.com>
2023-12-05 17:17:32 -0800

    Updated leveled spells array to hold the spell's level

--- src/app/char-creation-spells-tab/char-creation-spells-tab.component.ts
+++ src/app/char-creation-spells-tab/char-creation-spells-tab.component.ts
@@ -56,15 +56,15 @@ export class CharCreationSpellsTabComponent implements OnInit {
 
   //adds / removes user-selected spell to / from the relevant class's spell list
   //creates a spell list if it does not yet exist
-  updateLeveledSpells(classIndex: string, spellIndex: string, spellName: string) {
+  updateLeveledSpells(classIndex: string, spellIndex: string, spellName: string, spellLevel: number) {
     let classSpellcasterInfo = this.currentChar.classes[this.findRelevantIndex(classIndex)]!.spellcasterInfo!;
 
     if(classSpellcasterInfo.spellsKnown == null) {
       classSpellcasterInfo.spellsKnown = [];
     }
 
-    if(this.removeIfPresent(spellIndex, classSpellcasterInfo.spellsKnown)) {6
-      classSpellcasterInfo.spellsKnown.push([spellIndex, spellName]);
+    if(this.removeIfPresent(spellIndex, classSpellcasterInfo.spellsKnown)) {
+      classSpellcasterInfo.spellsKnown.push([spellIndex, spellName, spellLevel]);
     }
 
     console.log(classSpellcasterInfo.spellsKnown);
@@ -193,7 +193,7 @@ export class CharCreationSpellsTabComponent implements OnInit {
             if(spellLevel > 0) {
               this.dndApiService.DetermineAvailableSpellsAtSpellLevel(charClass.classIndex, String(index+1)).subscribe((spells) => {
                 spells.results.forEach((spell: any) => {
-                  tempSpells.push([spell.index, spell.name]);
+                  tempSpells.push([spell.index, spell.name, (index+1)]);
                 })
               })
             }
